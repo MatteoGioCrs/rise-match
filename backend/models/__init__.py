ec83057a4475d0592bacd2ba7ab81e44309d4c46
Matteo Caruso <matteo.caruso06@gmail.com>
2026-04-05 15:59:53 -0400

    clean: reset to zero

--- backend/models/__init__.py
+++
@@ -1,9 +0,0 @@
-from .swimmer import User, SwimmerProfile, Performance
-from .university import University, RosterAthlete, ConferenceResult
-from .match import Match, EmailLog
-
-__all__ = [
-    "User", "SwimmerProfile", "Performance",
-    "University", "RosterAthlete", "ConferenceResult",
-    "Match", "EmailLog",
-]
